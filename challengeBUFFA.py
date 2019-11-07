ef2399ba40607e2a1f13a69c1b4f91e7ff6bbfa1
brunoadbuffa <57489348+brunoadbuffa@users.noreply.github.com>
2019-11-07 17:05:13 -0300

    Add files via upload

---
+++ challengeBUFFA.py
@@ -0,0 +1,32 @@
+import numpy as np
+np.seterr(all='raise')
+from scipy.spatial.distance import cdist, euclidean
+
+def k_meansMODIF(X, k, tol=1e-4, n_iter=50):
+    n_samples, n_dim = X.shape    
+    # D² weighting initialization
+    centroids = D2weighting(X, k)
+
+    distortion, prev_distortion = 0., 1e6
+    for i in range(n_iter):
+        assignments = np.argmin(cdist(X, centroids),1)
+        centroids = np.array([np.mean(X[assignments == l], 0) for l in range(k)])
+        distortion = 0.
+        for j in range(n_samples):
+            distortion += euclidean(X[j], centroids[assignments[j]])
+        distortion /= n_samples
+        if ((prev_distortion - distortion) / (prev_distortion + 2**-23)) < tol:
+            print('done')
+            break
+        prev_distortion = distortion
+    print('iterations ={}, error ={:.3e}'.format(i+1, distortion))
+    return centroids, distortion
+
+def D2weighting(X, k):
+    centroids = X[np.random.choice(range(X.shape[0]), size=1)]
+    for j in range(1, k):
+        D = np.min(cdist(X, centroids, metric='sqeuclidean'),1) #D=[D(x1)² D(x2)²...D(xn)²] (dist² de c/x al centroide más cercano) 
+        pvals = D / sum(D) #arreglo con las prob de cada x de convertirse en el próximo centroide
+        centroids = np.vstack([centroids, X[np.where(np.random.multinomial(1,pvals))[0]]]) #agrego el nuevo centroide como fila de centroids
+    return centroids
+    
